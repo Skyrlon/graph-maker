f4cdf786faafba34a9d3c456fa79b224ae534047
Antoine Drouault <antoinedrouault@hotmail.com>
2022-09-28 23:59:51 +0200

    Add data inputs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import "./App.css";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import DataInputs from "./DataInputs";
+import styled from "styled-components";
 
-import { AppBar, Toolbar, Typography } from "@mui/material";
+const StyledBox = styled(Box)`
+  display: grid;
+  grid-template-columns: 80% 20%;
+  grid-template-rows: 3fr 1fr;
+  gap: 0px 0px;
+  grid-template-areas:
+    "graph data-inputs"
+    "save data-inputs";
+`;
 
 function App() {
   return (
     <div className="App">
-      <AppBar component="nav">
+      <AppBar component="nav" position="sticky">
         <Toolbar>
           <Typography variant="h6" component="div">
             Graph Maker
           </Typography>
         </Toolbar>
       </AppBar>
+      <StyledBox>
+        <DataInputs />
+      </StyledBox>
     </div>
   );
 }
---
+++ src/DataInputs.jsx
@@ -0,0 +1,37 @@
+import { Box, TextField } from "@mui/material";
+import { useState } from "react";
+import styled from "styled-components";
+
+const StyledDataInputs = styled(Box)`
+  grid-area: data-inputs;
+  border: 1px solid;
+`;
+
+function DataInputs() {
+  const [inputs, setInputs] = useState([{ position: 0, value: "" }]);
+
+  const handleChange = (e, position) => {
+    const inputToModify = inputs.find((input) => input.position === position);
+    let inputsModified = inputs;
+    inputsModified.splice(inputs.indexOf(inputToModify), 1, {
+      position,
+      value: e.target.value,
+    });
+    setInputs(inputsModified);
+  };
+
+  return (
+    <StyledDataInputs>
+      {inputs.map((input) => (
+        <TextField
+          key={input.position}
+          type="number"
+          value={input.value}
+          onChange={(e) => handleChange(e, input.position)}
+        />
+      ))}
+    </StyledDataInputs>
+  );
+}
+
+export default DataInputs;
