c0d4bda27cecbc0e7de363e52843ad8185e093a3
Antoine Drouault <antoinedrouault@hotmail.com>
2022-10-13 23:27:09 +0200

    Remove magical numbers

--- src/Graph.jsx
+++ src/Graph.jsx
@@ -7,13 +7,19 @@ const StyledGraph = styled.div`
 `;
 
 function Graph({ data }) {
-  const findOrderOfMagnitude = (number) => {
-    return Math.pow(10, Math.ceil(number).toString().length - 1);
-  };
+  const axisLength = 1000;
+
+  const axisMargin = axisLength * 0.1;
+
+  const imageLength = 2 * axisMargin + axisLength;
 
   const [xAxisAmplitude, setXAxisAmplitude] = useState();
 
   const [yAxisAmplitude, setYAxisAmplitude] = useState();
+
+  const findOrderOfMagnitude = (number) => {
+    return Math.pow(10, Math.ceil(number).toString().length - 1);
+  };
 
   useEffect(() => {
     const calculateAmplitude = (lowestValue, largestValue) => {
@@ -52,26 +58,34 @@ function Graph({ data }) {
       {data && (
         <svg
           width="50%"
-          viewBox="0 0 1200 1200"
+          viewBox={`0 0 ${imageLength} ${imageLength}`}
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
             fill="none"
             stroke="black"
             strokeWidth={10}
-            d="M 100,100 L 100,1150 M 50,1100 L 1100,1100"
+            d={`M ${axisMargin},${axisMargin} L ${axisMargin},${
+              imageLength - axisMargin / 2
+            } M ${axisMargin / 2},${axisMargin + axisLength} L ${
+              axisMargin + axisLength
+            },${axisMargin + axisLength}`}
           />
           <path
             fill="none"
             stroke="red"
             strokeWidth={5}
-            d={`M ${data[0].x * (1000 / xAxisAmplitude) + 100},${
-              1100 - data[0].y * (1000 / yAxisAmplitude)
+            d={`M ${data[0].x * (axisLength / xAxisAmplitude) + axisMargin},${
+              axisMargin +
+              axisLength -
+              data[0].y * (axisLength / yAxisAmplitude)
             } ${data
               .map(
                 (pos) =>
-                  `L ${pos.x * (1000 / xAxisAmplitude) + 100},${
-                    1100 - pos.y * (1000 / yAxisAmplitude)
+                  `L ${pos.x * (axisLength / xAxisAmplitude) + axisMargin},${
+                    axisMargin +
+                    axisLength -
+                    pos.y * (axisLength / yAxisAmplitude)
                   }`
               )
               .join(" ")}`}
