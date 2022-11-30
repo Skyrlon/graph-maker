1d88bf60f49986d01e1800112f3e4df77aa58f49
Antoine Drouault <antoinedrouault@hotmail.com>
2022-11-30 21:55:17 +0100

    Move svg element and parent inside Graph component

--- src/Graph.jsx
+++ src/Graph.jsx
@@ -1,16 +1,76 @@
 import styled from "styled-components";
 import LinearGraph from "./LinearGraph";
+import { useCallback } from "react";
 
 const StyledGraph = styled.div`
   grid-area: graph;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  .container {
+    width: 50%;
+  }
+  svg {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 function Graph({ data, sendSvgData }) {
+  const axisLength = 1000;
+
+  const axisMargin = axisLength * 0.3;
+
+  const imageLength = 2 * axisMargin + axisLength;
+
+  const axisStrokeWidth = axisLength / 100;
+
+  const textSize = imageLength / 40;
+
+  const graphColor = "black";
+
+  const graphData = {
+    axisLength,
+    axisMargin,
+    imageLength,
+    axisStrokeWidth,
+    textSize,
+    graphColor,
+  };
+
+  const handleSvg = useCallback(
+    (node) => {
+      sendSvgData({
+        width: graphData.imageLength,
+        height: graphData.imageLength,
+        node,
+      });
+    },
+    // eslint-disable-next-line
+    []
+  );
+
   return (
     <StyledGraph>
-      {data.graphType === "linear" && (
-        <LinearGraph data={data} sendSvgData={sendSvgData} />
-      )}
+      <div className="container">
+        <svg
+          viewBox={`0 0 ${imageLength} ${imageLength}`}
+          width={imageLength}
+          height={imageLength}
+          xmlns="http://www.w3.org/2000/svg"
+          ref={handleSvg}
+        >
+          {data.graphType === "linear" && (
+            <LinearGraph
+              data={data}
+              sendSvgData={sendSvgData}
+              graphData={graphData}
+            />
+          )}
+        </svg>
+      </div>
     </StyledGraph>
   );
 }
--- src/LinearGraph.jsx
+++ src/LinearGraph.jsx
@@ -1,35 +1,6 @@
-import { useEffect, useState, useCallback } from "react";
-import styled from "styled-components";
+import { useEffect, useState } from "react";
 
-const StyledLinearGraph = styled.div`
-  grid-area: graph;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  .container {
-    width: 50%;
-  }
-  svg {
-    position: relative;
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-function LinearGraph({ data, sendSvgData }) {
-  const axisLength = 1000;
-
-  const axisMargin = axisLength * 0.3;
-
-  const imageLength = 2 * axisMargin + axisLength;
-
-  const axisStrokeWidth = axisLength / 100;
-
-  const textSize = imageLength / 40;
-
-  const graphColor = "black";
-
+export default function LinearGraph({ data, graphData }) {
   const [axis, setAxis] = useState(undefined);
 
   const findOrderOfMagnitude = (number) => {
@@ -66,9 +37,9 @@ function LinearGraph({ data, sendSvgData }) {
     const lowestValue = getLowestValues(0)[0];
     const referenceValue = lowestValue < 0 ? lowestValue : 0;
     return (
-      axisMargin +
+      graphData.axisMargin +
       Math.abs(Number(number) - referenceValue) *
-        (axisLength / axis.x.amplitude)
+        (graphData.axisLength / axis.x.amplitude)
     );
   };
 
@@ -76,9 +47,9 @@ function LinearGraph({ data, sendSvgData }) {
     const largestY = getLargestValues(1)[0];
     const referenceValue = largestY > 0 ? largestY : 0;
     return (
-      axisMargin +
+      graphData.axisMargin +
       Math.abs(Number(number) - referenceValue) *
-        (axisLength / axis.y.amplitude)
+        (graphData.axisLength / axis.y.amplitude)
     );
   };
 
@@ -131,128 +102,109 @@ function LinearGraph({ data, sendSvgData }) {
     [data]
   );
 
-  const handleSvg = useCallback(
-    (node) => {
-      sendSvgData({
-        width: imageLength,
-        height: imageLength,
-        node,
-      });
-    },
-    // eslint-disable-next-line
-    []
-  );
-
   return (
-    <StyledLinearGraph>
-      {data && axis && (
-        <div className="container">
-          <svg
-            viewBox={`0 0 ${imageLength} ${imageLength}`}
-            width={imageLength}
-            height={imageLength}
-            xmlns="http://www.w3.org/2000/svg"
-            ref={handleSvg}
+    data &&
+    axis && (
+      <>
+        <defs>
+          <marker
+            id="arrow-head"
+            orient="auto-start-reverse"
+            markerWidth={graphData.axisStrokeWidth}
+            markerHeight={graphData.axisStrokeWidth}
+            refX="0.1"
+            refY="2"
           >
-            <defs>
-              <marker
-                id="arrow-head"
-                orient="auto-start-reverse"
-                markerWidth={axisStrokeWidth}
-                markerHeight={axisStrokeWidth}
-                refX="0.1"
-                refY="2"
-              >
-                <path d="M0,0 V4 L2,2 Z" fill={graphColor} />
-              </marker>
-            </defs>
+            <path d="M0,0 V4 L2,2 Z" fill={graphData.graphColor} />
+          </marker>
+        </defs>
 
-            <path
-              markerStart="url(#arrow-head)"
-              markerEnd="url(#arrow-head)"
-              fill="none"
-              stroke={graphColor}
-              strokeWidth={axisStrokeWidth}
-              d={`M ${axisMargin / 2},${getPositionY(0)} 
-                  L ${axisMargin * 1.5 + axisLength},${getPositionY(0)}`}
-            />
-            <path
-              markerStart="url(#arrow-head)"
-              markerEnd="url(#arrow-head)"
-              fill="none"
-              stroke={graphColor}
-              strokeWidth={axisStrokeWidth}
-              d={`M ${getPositionX(0)},${axisMargin / 2} 
-                  L ${getPositionX(0)},${imageLength - axisMargin / 2} 
+        <path
+          markerStart="url(#arrow-head)"
+          markerEnd="url(#arrow-head)"
+          fill="none"
+          stroke={graphData.graphColor}
+          strokeWidth={graphData.axisStrokeWidth}
+          d={`M ${graphData.axisMargin / 2},${getPositionY(0)} 
+                  L ${
+                    graphData.axisMargin * 1.5 + graphData.axisLength
+                  },${getPositionY(0)}`}
+        />
+        <path
+          markerStart="url(#arrow-head)"
+          markerEnd="url(#arrow-head)"
+          fill="none"
+          stroke={graphData.graphColor}
+          strokeWidth={graphData.axisStrokeWidth}
+          d={`M ${getPositionX(0)},${graphData.axisMargin / 2} 
+                  L ${getPositionX(0)},${
+            graphData.imageLength - graphData.axisMargin / 2
+          } 
               `}
-            />
+        />
 
-            {axis.x.values.map(
-              (v) =>
-                v !== 0 && (
-                  <g
-                    key={v}
-                    transform={`translate(${getPositionX(v)}, ${getPositionY(
-                      0
-                    )})`}
-                  >
-                    <rect
-                      fill={graphColor}
-                      x={-textSize / 8}
-                      y={-textSize / 2}
-                      width={textSize / 4}
-                      height={textSize}
-                    />
-                    <text
-                      x={0}
-                      y={textSize * 2}
-                      fontSize={textSize}
-                      textAnchor="middle"
-                      color={graphColor}
-                    >
-                      {v}
-                    </text>
-                  </g>
-                )
+        {axis.x.values.map(
+          (v) =>
+            v !== 0 && (
+              <g
+                key={v}
+                transform={`translate(${getPositionX(v)}, ${getPositionY(0)})`}
+              >
+                <rect
+                  fill={graphData.graphColor}
+                  x={-graphData.textSize / 8}
+                  y={-graphData.textSize / 2}
+                  width={graphData.textSize / 4}
+                  height={graphData.textSize}
+                />
+                <text
+                  x={0}
+                  y={graphData.textSize * 2}
+                  fontSize={graphData.textSize}
+                  textAnchor="middle"
+                  color={graphData.graphColor}
+                >
+                  {v}
+                </text>
+              </g>
+            )
+        )}
+
+        {axis.y.values.map(
+          (v) =>
+            v !== 0 && (
+              <g
+                key={v}
+                transform={`translate(${getPositionX(0)}, ${getPositionY(v)})`}
+              >
+                <rect
+                  fill={graphData.graphColor}
+                  x={-graphData.textSize / 2}
+                  y={-graphData.textSize / 8}
+                  width={graphData.textSize}
+                  height={graphData.textSize / 4}
+                />
+                <text
+                  x={-graphData.textSize}
+                  y={graphData.textSize / 3}
+                  fontSize={graphData.textSize}
+                  textAnchor="middle"
+                >
+                  {v}
+                </text>
+              </g>
+            )
+        )}
+
+        {data.sets.map((set) => (
+          <path
+            key={set.id}
+            fill="none"
+            stroke={set.color}
+            strokeWidth={graphData.axisStrokeWidth / 2}
+            d={`M ${getPositionX(set.groups[0].inputs[0])},${getPositionY(
+              set.groups[0].inputs[1]
             )}
-
-            {axis.y.values.map(
-              (v) =>
-                v !== 0 && (
-                  <g
-                    key={v}
-                    transform={`translate(${getPositionX(0)}, ${getPositionY(
-                      v
-                    )})`}
-                  >
-                    <rect
-                      fill={graphColor}
-                      x={-textSize / 2}
-                      y={-textSize / 8}
-                      width={textSize}
-                      height={textSize / 4}
-                    />
-                    <text
-                      x={-textSize}
-                      y={textSize / 3}
-                      fontSize={textSize}
-                      textAnchor="middle"
-                    >
-                      {v}
-                    </text>
-                  </g>
-                )
-            )}
-
-            {data.sets.map((set) => (
-              <path
-                key={set.id}
-                fill="none"
-                stroke={set.color}
-                strokeWidth={axisStrokeWidth / 2}
-                d={`M ${getPositionX(set.groups[0].inputs[0])},${getPositionY(
-                  set.groups[0].inputs[1]
-                )}
               ${set.groups
                 .map(
                   (group) =>
@@ -261,72 +213,76 @@ function LinearGraph({ data, sendSvgData }) {
                     )}`
                 )
                 .join(" ")}`}
+          />
+        ))}
+
+        {data.sets.map((set) =>
+          set.groups.map((group) => (
+            <circle
+              key={group.id}
+              cx={getPositionX(group.inputs[0])}
+              cy={getPositionY(group.inputs[1])}
+              r={graphData.axisStrokeWidth}
+              fill={graphData.graphColor}
+            />
+          ))
+        )}
+
+        <text
+          x={graphData.imageLength - graphData.axisMargin / 2}
+          y={graphData.axisMargin / 3 + getPositionY(0)}
+          style={{ fontSize: graphData.textSize }}
+          color={graphData.graphColor}
+        >
+          {data.axis.first}
+        </text>
+        <text
+          textAnchor="middle"
+          x={getPositionX(0)}
+          y={graphData.axisMargin / 3}
+          fontSize={graphData.textSize}
+          color={graphData.graphColor}
+        >
+          {data.axis.second}
+        </text>
+        <text
+          textAnchor="middle"
+          x={graphData.imageLength / 2}
+          y={graphData.imageLength - graphData.axisMargin / 4}
+          fontSize={graphData.textSize}
+          color={graphData.graphColor}
+        >
+          {data.title}
+        </text>
+        {data.sets.length > 1 &&
+          data.sets.map((set, index) => (
+            <g key={set.id}>
+              <rect
+                fill={set.color}
+                x={
+                  graphData.imageLength -
+                  graphData.axisMargin / 2 -
+                  graphData.textSize
+                }
+                y={
+                  graphData.axisMargin +
+                  graphData.textSize * index -
+                  graphData.textSize / 2
+                }
+                width={graphData.textSize / 2}
+                height={graphData.textSize / 2}
               />
-            ))}
-
-            {data.sets.map((set) =>
-              set.groups.map((group) => (
-                <circle
-                  key={group.id}
-                  cx={getPositionX(group.inputs[0])}
-                  cy={getPositionY(group.inputs[1])}
-                  r={axisStrokeWidth}
-                  fill={graphColor}
-                />
-              ))
-            )}
-
-            <text
-              x={imageLength - axisMargin / 2}
-              y={axisMargin / 3 + getPositionY(0)}
-              style={{ fontSize: textSize }}
-              color={graphColor}
-            >
-              {data.axis.first}
-            </text>
-            <text
-              textAnchor="middle"
-              x={getPositionX(0)}
-              y={axisMargin / 3}
-              fontSize={textSize}
-              color={graphColor}
-            >
-              {data.axis.second}
-            </text>
-            <text
-              textAnchor="middle"
-              x={imageLength / 2}
-              y={imageLength - axisMargin / 4}
-              fontSize={textSize}
-              color={graphColor}
-            >
-              {data.title}
-            </text>
-            {data.sets.length > 1 &&
-              data.sets.map((set, index) => (
-                <g key={set.id}>
-                  <rect
-                    fill={set.color}
-                    x={imageLength - axisMargin / 2 - textSize}
-                    y={axisMargin + textSize * index - textSize / 2}
-                    width={textSize / 2}
-                    height={textSize / 2}
-                  />
-                  <text
-                    x={imageLength - axisMargin / 2}
-                    y={axisMargin + textSize * index}
-                    fontSize={textSize}
-                    color={graphColor}
-                  >
-                    {set.name}
-                  </text>
-                </g>
-              ))}
-          </svg>
-        </div>
-      )}
-    </StyledLinearGraph>
+              <text
+                x={graphData.imageLength - graphData.axisMargin / 2}
+                y={graphData.axisMargin + graphData.textSize * index}
+                fontSize={graphData.textSize}
+                color={graphData.graphColor}
+              >
+                {set.name}
+              </text>
+            </g>
+          ))}
+      </>
+    )
   );
 }
-
-export default LinearGraph;
